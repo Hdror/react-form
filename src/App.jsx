2c3f484bec77bfbc5de531b62755a8a1e3701b42
Dror Handler <hdror507@gmail.com>
2024-02-23 00:27:10 +0200

    creating-form-validation

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,16 @@ function App() {
     lastName: '',
     id: '',
     email: '',
-    isMale: false,
-    isFemale: false,
     birthDayDate: '',
+    gender: ''
   })
+
+  const [validation, setValidation] = useState({
+    firstName: { isValid: true, message: '' },
+    lastName: { isValid: true, message: '' },
+    id: { isValid: true, message: '' },
+    email: { isValid: true, message: '' },
+  });
 
   const clearForm = () => {
     setForm({
@@ -19,22 +25,95 @@ function App() {
       lastName: '',
       id: '',
       email: '',
-      isMale: false,
-      isFemale: false,
+      gender: '',
       birthDayDate: '',
-    })
-  }
+    });
+
+    setValidation({
+      firstName: { isValid: true, message: '' },
+      lastName: { isValid: true, message: '' },
+      id: { isValid: true, message: '' },
+      email: { isValid: true, message: '' },
+    });
+  };
 
   const handleChange = (event) => {
     const name = event.target.name;
-    const value = event.target.value;
-    setForm(values => ({ ...values, [name]: value }))
-  }
+    console.log(name);
+    console.log(`${event.target.checked}`);
+    const value = event.target.type === 'radio' ? event.target.id : event.target.value;
+    let isValid = true;
+    let message = '';
+    // Perform validation based on the field name
+    switch (name) {
+      case 'firstName':
+        isValid = checkValidName(value);
+        message = isValid ? '' : 'שם פרטי לא תקין';
+        break;
+      case 'lastName':
+        isValid = checkValidName(value);
+        message = isValid ? '' : 'שם משפחה לא תקין';
+        break;
+      case 'id':
+        isValid = isIsraeliIdNumber(value);
+        message = isValid ? '' : 'תעודת זהות לא תקינה';
+        break;
+      case 'email':
+        isValid = validateEmail(value);
+        message = isValid ? '' : 'דוא"ל לא תקין';
+        break;
+      default:
+        break;
+    }
+
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+    setValidation((prevValidation) => ({ ...prevValidation, [name]: { isValid, message } }));
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(form);
+    event.preventDefault();
+
+    // Perform final validation before submitting the form
+
+    if (isFormValid()) {
+      console.log('Form submitted:', form);
+    } else {
+      console.log('Form contains errors. Please fix them before submitting.');
+    }
+  };
+
+
+  const checkValidName = (str) => {
+    const reg = new RegExp('^[\u0590-\u05FF]+$')
+    if (!reg.test(str) || str.length < 2) return false
+    return true
   }
+
+  const validateEmail = (email) => {
+    console.log(email);
+    var re = new RegExp('[/\S+@\S+\.\S+/]');
+    return re.test(email);
+  }
+
+  const isIsraeliIdNumber = (id) => {
+    id = String(id).trim();
+
+    if (id.length > 9 || isNaN(id)) return false;
+    id = id.length < 9 ? ("00000000" + id).slice(-9) : id;
+
+    const sum = id.split('').reduce((counter, digit, i) => {
+      const step = Number(digit) * ((i % 2) + 1);
+      return counter + (step > 9 ? step - 9 : step);
+    }, 0);
+
+    return sum % 10 === 0;
+  }
+
+  const isFormValid = () => {
+    return Object.values(validation).every((field) => field.isValid);
+  }
+
+
 
   const personalFormFields =
     [
@@ -47,28 +126,65 @@ function App() {
   return (
     <div className="app">
       <form onSubmit={handleSubmit} className='form'>
+        <h3>פרטים אישיים</h3>
         <div className="personal-details-container">
           {personalFormFields.map(fieldItem => {
             return <label key={fieldItem.field}>{fieldItem.field}
-              <input onChange={handleChange} value={form[`${fieldItem.fieldName}`]} placeholder={fieldItem.field} name={fieldItem.fieldName} type={fieldItem.type} aria-label={fieldItem.field} required/>
+              <input
+                onChange={handleChange}
+                value={form[`${fieldItem.fieldName}`]}
+                placeholder={fieldItem.field}
+                name={fieldItem.fieldName}
+                type={fieldItem.type}
+                aria-label={fieldItem.field}
+                required />
+              {!validation[fieldItem.fieldName].isValid && (
+                <span className="error-message">{validation[fieldItem.fieldName].message}</span>
+              )}
             </label>
           })}
         </div>
+        <h3>מין</h3>
         <div className="gender-birthday-container">
           <div className="gender-container">
             <label>זכר
-              <input onChange={handleChange} placeholder='זכר' type="checkbox" aria-label='זכר' />
+              <input
+                id='male'
+                checked={form.gender === 'male'}
+                name="gender"
+                onChange={handleChange}
+                placeholder='זכר'
+                type="radio"
+                aria-label='זכר' />
             </label>
             <label>נקבה
-              <input onChange={handleChange} placeholder='נקבה' type="checkbox" aria-label='נקבה' />
+              <input
+                id='female'
+                checked={form.gender === 'female'}
+                name="gender"
+                onChange={handleChange}
+                placeholder='נקבה'
+                type="radio"
+                aria-label='נקבה' />
             </label>
           </div>
-          <label className='birthday-label'>תאריך לידה
-            <input onChange={handleChange} value={form.birthDayDate} placeholder='תאריך לידה' name='birthDayDate' type="date" aria-label='תאריך לידה' required />
+          <label className="birthday-label">תאריך לידה
+            <input
+              onChange={handleChange}
+              value={form.birthDayDate}
+              placeholder='תאריך לידה'
+              name='birthDayDate'
+              type="date"
+              aria-label='תאריך לידה'
+              required />
           </label>
         </div>
         <div className="buttons-container">
-          <button  type='submit' aria-label='סיום'>סיום</button>
+          <button
+            className="submit-btn"
+            disabled={!isFormValid()}
+            type='submit'
+            aria-label='סיום'>סיום</button>
           <button onClick={clearForm} type='button' aria-label='איפוס טופס'>איפוס טופס</button>
         </div>
       </form>
